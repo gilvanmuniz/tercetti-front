c4f3bb63a0a8862c0cd6bb82be9e58ee8a5c7bf6
Gilvan <camisetaecia@live.com>
2019-11-09 21:36:46 -0300

    obtendo retorno de token da api

---
+++ src/modules/auth/pages/Login.vue
@@ -0,0 +1,73 @@
+<template>
+<div class="login-page">
+     <form @submit.prevent="submit()">
+    <div class="login-page">
+      <div class="card">
+        <div class="card-header">
+          Login
+        </div>
+        <div class="card-body">
+          <div class="form-group">
+            <input
+              required
+              type="email"
+              v-model="form.username"
+              class="form-control"
+              placeholder="E-mail"
+            >
+          </div>
+          <div class="form-group">
+            <input
+              required
+              type="password"
+              placeholder="Senha"
+              class="form-control"
+              v-model="form.password"
+            >
+          </div>
+
+          <button class="btn btn-primary w-100">
+            Entrar
+          </button>
+        </div>
+      </div>
+    </div>
+  </form>
+</div>
+</template>
+
+<script>
+import { mapActions } from 'vuex'
+
+export default {
+  data: () => ({
+    form: {
+      username: '',
+      password: ''
+    }
+  }),
+  methods: {
+    ...mapActions('auth', ['ActionDoLogin']),
+    async submit () {
+      try {
+        await this.ActionDoLogin(this.form)
+        this.$router.push({ name: 'home' })
+      } catch (err) {
+        alert(err.data ? err.data.message : 'Não foi possível fazer o login')
+      }
+    }
+  }
+}
+</script>
+
+<style scoped lang="scss">
+.login-page {
+  height: 100vh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+.card {
+    width: 100%;
+  }
+}
+</style>
