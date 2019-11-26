dc63f31a4b65a98d15569ae001addbde05ab6a7e
Gilvan <camisetaecia@live.com>
2019-11-26 20:07:12 -0300

    autentication realized

---
+++ src/router/beforeEach.js
@@ -0,0 +1,18 @@
+import store from '../store'
+export default async (to, from, next) => {
+  document.title = `${to.name} - Senhas`
+  if (to.name !== 'login' && !store.getters['auth/hasToken']) {
+    try {
+      await store.dispatch('auth/ActionCheckToken')
+      next({ name: to.name })
+    } catch (err) {
+      next({ name: 'login' })
+    }
+  } else {
+    if (to.name === 'login' && store.getters['auth/hasToken']) {
+      next({ name: 'home' })
+    } else {
+      next()
+    }
+  }
+}
--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,12 @@
 import Vue from 'vue'
 import routes from './routes'
 import VueRouter from 'vue-router'
+import beforeEach from './beforeEach'
 
 Vue.use(VueRouter)
 
-export default new VueRouter({ routes })
+const router = new VueRouter({ routes })
+
+router.beforeEach(beforeEach)
+
+export default router
